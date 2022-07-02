4cfe26b655651d3550c7c8160647a938e3908f0e
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-07-02 15:24:07 -0300

    Create exercises routers

---
+++ src/routers/exercises.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from src.database.database import database, exercises
+
+router = APIRouter(
+    prefix='/exercises',
+    tags=['exercises']
+)
+
+
+class Exercise(BaseModel):
+    name: str
+    muscle_type: int
+    level: int
+
+
+@router.post('/new')
+async def register_tasks(exercise: Exercise):
+    last_record_id = await database.execute(exercise.insert().values(**exercise.dict()))
+    return {**exercise.dict(), 'id': last_record_id}
+
+
+@router.get('/')
+async def get_exercises():
+  return await database.fetch_all(exercises.select())

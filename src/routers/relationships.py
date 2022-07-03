91581aa693d9ac3911538a11eeaf8e3965a186b0
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-07-03 15:02:56 -0300

    Implement exercises select from relationship with training

--- src/routers/relationships.py
+++ src/routers/relationships.py
@@ -21,14 +21,22 @@ async def register_relationship(relation: Relationship):
 
 @router.get('/')
 async def get_relationship():
-  return await database.fetch_all(relationships.select())
+    return await database.fetch_all(relationships.select())
 
 
 @router.get('/single')
 async def get_one_relationship(relationship_id: int):
-  return await database.fetch_one(relationships.select().where( relationships.columns.id == relationship_id ))
+    return await database.fetch_one(relationships.select().where( relationships.columns.id == relationship_id ))
 
 
 @router.get('/group')
 async def get_relationship_by_training(training_id: int):
-  return await database.fetch_all(relationships.select().where( relationships.columns.training_id == training_id ))
+    return await database.fetch_all(relationships.select().where( relationships.columns.training_id == training_id ))
+
+@router.get('/exercises')
+async def get_exercises_by_training(training_id: int):
+    relationships_by_training = await database.fetch_all(relationships.select().where( relationships.columns.training_id == training_id ))
+    training_exercises = []
+    for exercise_id in relationships_by_training:
+        training_exercises.append(await database.fetch_one(exercises.select().where( exercises.columns.id == exercise_id[1])))
+    return training_exercises
